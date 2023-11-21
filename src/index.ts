e68f7df997cab80458db6eba9f0a3a97e133f24f
AniaPiWo <snowflake@wp.pl>
2023-11-21 17:33:40 +0100

    init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,108 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-
 import confetti from 'canvas-confetti';
+import { v4 as uuidv4 } from 'uuid';
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+interface Task {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+}
+
+const list = document.querySelector('#list') as HTMLUListElement;
+const form = document.querySelector('#new-task-form') as HTMLFormElement | null;
+const input = document.querySelector('#new-task-title') as HTMLInputElement;
+const mainInfo = document.querySelector('#empty') as HTMLParagraphElement;
+const tasks: Task[] = loadTasks();
+
+if (tasks.length === 0) mainInfo.textContent = 'No tasks!';
+
+tasks.forEach((task) => {
+  if (tasks.length > 0) mainInfo.classList.add('hidden');
+  addListItem(task);
+});
+
+form
+  ? addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      if (input?.value === '' || input?.value === null) return;
+
+      const newTask: Task = {
+        id: uuidv4(),
+        title: input.value,
+        completed: false,
+        createdAt: new Date(),
+      };
+      tasks.push(newTask);
+      addListItem(newTask);
+      input.value = '';
+    })
+  : null;
+
+function addListItem(task: Task) {
+  mainInfo.classList.add('hidden');
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  const buttonBox = document.createElement('div');
+  const deleteBtn = document.createElement('button');
+
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  checkbox.addEventListener('change', () => {
+    task.completed = !task.completed;
+    saveTasks();
+    if (task.completed) {
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: {
+          y: 0.6,
+        },
+      });
+      label.classList.add('completed');
+      item.classList.add('completed');
+    }
+  });
+
+  saveTasks();
+
+  buttonBox.classList.add('button-box');
+
+  deleteBtn.textContent = 'Delete';
+  deleteBtn.classList.add('delete');
+  deleteBtn.addEventListener('click', () => {
+    removeTask(task.id);
+    item.remove();
+    if (tasks.length === 0) mainInfo.classList.remove('hidden');
+    mainInfo.textContent = 'No tasks!';
+  });
+
+  label.append(task.title);
+  buttonBox.append(checkbox, deleteBtn);
+  item.append(label, buttonBox);
+  list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  console.log(tasks);
+}
+
+function removeTask(id: string) {
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+  if (taskIndex === -1) return;
+
+  tasks.splice(taskIndex, 1);
+  saveTasks();
+}
+
+function loadTasks(): Task[] {
+  const tasksJSON = localStorage.getItem('tasks');
+  if (tasksJSON === null) return [];
+  const savedTasks: Task[] = JSON.parse(tasksJSON);
+
+  return JSON.parse(tasksJSON);
+}
+
+//localStorage.clear();
